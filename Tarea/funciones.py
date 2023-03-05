22c8ae1a12d2fa92f946e06ea02d3114f03d2527
Jose Guachun <JoseGuachunS@hotmail.com>
2023-03-04 22:57:53 -0500

    Tarea

---
+++ Tarea/funciones.py
@@ -0,0 +1,35 @@
+from django.core.paginator import Paginator
+from datetime import datetime
+
+def Ruta(instance, file_name):
+    route='{}/{}/{}/{}'.format( 'users',instance.user.username,'profile_picture',file_name)
+    return route
+
+class MiPaginador(Paginator):
+    def __init__(self, object_list, per_page, orphans=0, allow_empty_first_page=True, rango=5):
+        super(MiPaginador, self).__init__(object_list, per_page, orphans=orphans, allow_empty_first_page=allow_empty_first_page)
+        self.rango = rango
+        self.paginas = []
+        self.primera_pagina = False
+        self.ultima_pagina = False
+
+    def rangos_paginado(self, pagina):
+        left = pagina - self.rango
+        right = pagina + self.rango
+        if left < 1:
+            left = 1
+        if right > self.num_pages:
+            right = self.num_pages
+        self.paginas = range(left, right + 1)
+        self.primera_pagina = True if left > 1 else False
+        self.ultima_pagina = True if right < self.num_pages else False
+        self.ellipsis_izquierda = left - 1
+        self.ellipsis_derecha = right + 1
+
+def generar_nombre(nombre, original):
+    ext = ""
+    if original.find(".") > 0:
+        ext = original[original.rfind("."):]
+    fecha = datetime.now().date()
+    hora = datetime.now().time()
+    return nombre + fecha.year.__str__() + fecha.month.__str__() + fecha.day.__str__() + hora.hour.__str__() + hora.minute.__str__() + hora.second.__str__() + ext.lower()
---
+++ Tarea/middleware.py
@@ -0,0 +1,58 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+class SignUpBlockMiddleware:
+    #Middleware de finalización de perfil.
+    # Asegúrese de que cada usuario que interactúa con la plataformatienen su foto de perfil y biografía.
+    def __init__(self, get_response):
+        """inicializacion del middleware"""
+        self.get_response =get_response
+    
+
+    def __call__(self, request):
+        """Código a ejecutar para cada solicitud antes de que se llame a 
+            la vista."""
+        if not request.user.is_anonymous:
+            if  not request.user.is_staff:
+                    if request.path in [reverse('tasks:signup'), reverse('tasks:home')]:
+                        return redirect('posts:feed')
+                    else:
+                        users=User.objects.all()
+                        for user in users:
+                            if request.path in[
+                                reverse('users:detail', args=[user.username] ),
+                                reverse('iteractions:messages', args=[user.username] ),
+                                reverse('iteractions:directs', args=[user.username] ),
+                                reverse('iteractions:newconversation', args=[user.username] ),
+                                ]:
+                                return redirect('users:validate_token')
+
+                        projects=Project.objects.all()
+                        for project in projects:
+                            if request.path in[
+                                reverse('posts:detail_project', args=[project.url, project.id] ),
+                                ]:
+                                return redirect('users:validate_token')  
+
+                        if request.path in [
+                             
+                            reverse('users:signup'), 
+                            reverse('users:update_profile'),
+                            reverse('users:update_user'),
+                            reverse('users:social_net'),
+                            reverse('users:change_password'),
+                            reverse('users:delete_user'),
+
+                            reverse('posts:feed'),  
+                            reverse('posts:list_project'), 
+                            reverse('posts:new_project'),
+
+                            reverse('iteractions:list_user'),
+                            reverse('iteractions:notification'),
+                            reverse('iteractions:send-direct'),
+                            
+                            ]:
+                            return redirect('users:validate_token')
+        response = self.get_response(request)
+        return response
